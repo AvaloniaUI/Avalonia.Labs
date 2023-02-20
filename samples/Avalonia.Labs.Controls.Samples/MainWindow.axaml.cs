6123482180e892e8297bef70cf5945c68ae870af
Max Katz <maxkatz6@outlook.com>
2023-02-20 17:12:46 -0500

    Add initial projects

---
+++ samples/Avalonia.Labs.Controls.Samples/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Labs.Controls.Samples;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ tests/Avalonia.Labs.Controls.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Avalonia.Labs.Controls.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
