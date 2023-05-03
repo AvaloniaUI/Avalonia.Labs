af46ac95a34107c7ef9dc811b3d82530fe3451dc
Emmanuel Hansen <emmausssss@gmail.com>
2023-05-03 14:56:05 +0000

    add virtualizing wrap panel

---
+++ samples/Avalonia.Labs.Catalog/ViewModels/VirtualizingWrapPanelViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Collections;
+using Avalonia.Labs.Catalog.Views;
+using ReactiveUI;
+
+namespace Avalonia.Labs.Catalog.ViewModels
+{
+    public class VirtualizingWrapPanelViewModel : ViewModelBase
+    {
+        private AvaloniaList<string>? _items;
+
+        static VirtualizingWrapPanelViewModel()
+        {
+            ViewLocator.Register(typeof(VirtualizingWrapPanelViewModel), () => new VirtualizingWrapPanelView());
+        }
+
+        public AvaloniaList<string>? Items
+        {
+            get => _items;
+            set => this.RaiseAndSetIfChanged(ref _items, value);
+        }
+
+        public VirtualizingWrapPanelViewModel()
+        {
+            Title = "Virtualizing WrapPanel";
+
+            _items = new AvaloniaList<string>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                _items.Add($"Box {i + 1}");
+            }
+        }
+    }
+}
---
+++ samples/Avalonia.Labs.Catalog/Views/VirtualizingWrapPanelView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Labs.Catalog.Views
+{
+    public partial class VirtualizingWrapPanelView : UserControl
+    {
+        public VirtualizingWrapPanelView()
+        {
+            InitializeComponent();
+        }
+    }
+}
