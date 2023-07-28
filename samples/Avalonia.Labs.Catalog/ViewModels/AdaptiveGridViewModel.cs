64a4225d19f8596779e23d7f2b388407eaed037e
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-07-28 18:39:45 +0200

    feat(Samples):  Add AdaptiveGrid sample

---
+++ samples/Avalonia.Labs.Catalog/ViewModels/AdaptiveGridViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Avalonia.Labs.Catalog.ViewModels;
+
+internal class AdaptiveGridViewModel : ViewModelBase
+{
+    static AdaptiveGridViewModel()
+    {
+        ViewLocator.Register(typeof(AdaptiveGridViewModel), () => new Views.AdaptiveGridView());
+    }
+
+    public AdaptiveGridViewModel()
+    {
+        Title = "AdaptiveGrid";
+    }
+
+    public IReadOnlyList<Step> Steps { get; } =
+    new Step[]
+    {
+            new(1,"Step","Registration"),
+            new(2,"Step 2","Fill in basic information"),
+            new(3,"Step 3","Upload file"),
+            new(4,"Step 4","Complete"),
+    };
+}
---
+++ samples/Avalonia.Labs.Catalog/Views/AdaptiveGridView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Labs.Catalog.Views;
+
+public partial class AdaptiveGridView : UserControl
+{
+    public AdaptiveGridView()
+    {
+        InitializeComponent();
+    }
+}
