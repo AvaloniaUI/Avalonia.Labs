daaa76468b8c0661b1b15c14708b2dbe55d96a69
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-10-05 17:34:46 +0200

    fix(Mobile): Samples content sizing

--- samples/Avalonia.Labs.Catalog/Views/WelcomeView.axaml.cs
+++ samples/Avalonia.Labs.Catalog/Views/WelcomeView.axaml.cs
@@ -1,12 +1,15 @@
+using System;
 using Avalonia.Controls;
 
 namespace Avalonia.Labs.Catalog.Views
 {
-    public partial class WelcomeView : UserControl
+    public partial class WelcomeView : ScrollViewer
     {
         public WelcomeView()
         {
             InitializeComponent();
         }
+
+        protected override Type StyleKeyOverride => typeof(ScrollViewer);
     }
 }
