c03ad582d3f82141e56cedf3f4d18d16fb3d8ca4
Max Katz <maxkatz6@outlook.com>
2024-01-28 14:41:37 -0800

    Re-port ExpressionBuilder, but now from WindowsCommunityToolkit

---
+++ src/Avalonia.Labs.ExpressionBuilder/ReferenceNodes/AmbientLightReferenceNode.cs
@@ -0,0 +1,47 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+#if AVALONIA_COMPOSITION_TODO
+using Avalonia.Rendering.Composition;
+
+namespace Avalonia.Labs.ExpressionBuilder
+{
+    /// <summary>
+    /// Class AmbientLightReferenceNode. This class cannot be inherited.
+    /// </summary>
+    /// <seealso cref="Avalonia.Labs.ExpressionBuilder.ReferenceNode" />
+    public sealed class AmbientLightReferenceNode : ReferenceNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AmbientLightReferenceNode"/> class.
+        /// </summary>
+        /// <param name="paramName">Name of the parameter.</param>
+        /// <param name="light">The light.</param>
+        internal AmbientLightReferenceNode(string paramName, AmbientLight light = null)
+            : base(paramName, light)
+        {
+        }
+
+        /// <summary>
+        /// Creates the target reference.
+        /// </summary>
+        /// <returns>AmbientLightReferenceNode.</returns>
+        internal static AmbientLightReferenceNode CreateTargetReference()
+        {
+            var node = new AmbientLightReferenceNode(null);
+            node.NodeType = ExpressionNodeType.TargetReference;
+
+            return node;
+        }
+
+        /// <summary>
+        /// Gets the color.
+        /// </summary>
+        /// <value>The color.</value>
+        public ColorNode Color
+        {
+            get { return ReferenceProperty<ColorNode>("Color"); }
+        }
+    }
+}
+#endif
