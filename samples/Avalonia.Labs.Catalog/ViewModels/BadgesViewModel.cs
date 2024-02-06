fc8b5de79f3adb1f7211e6f4c79c4a061b2e6231
Giuseppe Lippolis <workgroupengineering@outlook.com>
2024-02-06 10:56:11 +0100

    Add Sample

---
+++ samples/Avalonia.Labs.Catalog/ViewModels/BadgesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Labs.Catalog.Views;
+
+namespace Avalonia.Labs.Catalog.ViewModels;
+
+internal class BadgesViewModel:ViewModelBase
+{
+    static BadgesViewModel() 
+    {
+        ViewLocator.Register(typeof(BadgesViewModel), () => new BadgesView());
+    } 
+}
