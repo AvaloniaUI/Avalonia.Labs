e1fcd3753e2907452086ea7d06a4c2051df46d44
Emmanuel Hansen <emmausssss@gmail.com>
2024-06-11 07:59:40 -0400

    add linux support

---
+++ src/Avalonia.Labs.Notifications/Avalonia.Labs.Notifications/Linux/LinuxNativeNotificationManager.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Avalonia.Media.Imaging;
+using static Avalonia.Labs.Notifications.Linux.NativeInterop;
+
+namespace Avalonia.Labs.Notifications.Linux
+{
+    internal class LinuxNativeNotificationManager : INativeNotificationManager, IDisposable
+    {
+        public const string DefaultAction = "default";
+        public delegate void ActionDelegate(IntPtr notification, string action, IntPtr user_data);
+        private readonly Dictionary<uint, INativeNotification> _notifications = new Dictionary<uint, INativeNotification>();
+        private string _appName;
+        private readonly string? _appIcon;
+        private bool _isInitialized;
+        private Thread _loopThread;
+        private IntPtr _loop;
+        public ActionDelegate Callback = new ActionDelegate(Activated);
+
+        public LinuxNativeNotificationManager(string appName, string? appIcon)
+        {
+            _appName = appName;
+            _appIcon = appIcon;
+            ChannelManager = new LinuxNotificationChannelManager();
+        }
+
+        public event EventHandler<NativeNotificationCompletedEventArgs>? NotificationCompleted;
+
+        public IDictionary<uint, INativeNotification> ActiveNotifications => _notifications;
+
+        internal LinuxNotificationChannelManager ChannelManager { get; set; }
+
+        public string? AppIcon => _appIcon;
+
+        public string AppName { get => _appName; set => _appName = value; }
+
+        public INativeNotification? CreateNotification(string? category)
+        {
+            if (string.IsNullOrWhiteSpace(_appName) || !_isInitialized)
+            {
+                return null;
+            }
+            var channel = ChannelManager?.GetChannel(category ?? LinuxNotificationChannelManager.DefaultChannel) ??
+                ChannelManager?.AddChannel(new NotificationChannel(LinuxNotificationChannelManager.DefaultChannel, LinuxNotificationChannelManager.DefaultChannelLabel));
+
+            if (channel == null)
+            {
+                return null;
+            }
+            return new LinuxNativeNotification(channel, this);
+        }
+
+        internal unsafe void Initialize()
+        {
+            if (string.IsNullOrWhiteSpace(_appName) || _isInitialized)
+            {
+                return;
+            }
+
+            try
+            {
+                _isInitialized = notify_init(_appName);
+
+                if (_isInitialized && _loopThread == null)
+                {
+                    _loopThread = new Thread(() =>
+                    {
+                        _loop = g_main_loop_new(IntPtr.Zero, false);
+                        g_main_loop_run(_loop);
+                    });
+
+                    _loopThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        internal void Show(LinuxNativeNotification notification)
+        {
+            if (!_isInitialized)
+            {
+                return;
+            }
+
+            if (notification != null)
+            {
+                IntPtr error = IntPtr.Zero;
+                var successfull = notify_notification_show(notification.NativeHandle, ref error);
+
+                if (!successfull)
+                {
+                    var details = GError.FromPointer(error);
+
+                    CloseNotification(notification);
+
+                    g_error_free(error);
+                }
+                else
+                {
+                    _notifications.TryAdd(notification.Id, notification);
+                }
+                notification.DeleteNativeResources();
+            }
+        }
+
+        public void CloseAll()
+        {
+            foreach (var notification in _notifications)
+            {
+                CloseNotification(notification.Value as LinuxNativeNotification);
+            }
+
+            _notifications.Clear();
+        }
+
+
+        internal static void CloseNotification(LinuxNativeNotification? notification)
+        {
+            if (notification != null && notification.NativeHandle != default)
+            {
+                IntPtr error = default;
+                notify_notification_close(notification.NativeHandle, ref error);
+
+                if (error != default)
+                    g_error_free(error);
+
+                notification.NativeHandle = default;
+            }
+        }
+
+
+        private static void Activated(IntPtr notification, string action, IntPtr user_data)
+        {
+            var id = (uint)user_data;
+
+            if (NativeNotificationManager.Current is LinuxNativeNotificationManager manager)
+            {
+                if (manager._notifications.ContainsKey(id) && manager._notifications.Remove(id, out var nativeNotification))
+                {
+                    var eventArgs = new NativeNotificationCompletedEventArgs()
+                    {
+                        NotificationId = id,
+                        IsActivated = action == DefaultAction,
+                        ActionTag = action == DefaultAction ? null : action,
+                    };
+
+                    manager.NotificationCompleted?.Invoke(manager, eventArgs);
+
+                    manager.Close(nativeNotification as LinuxNativeNotification);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_loop != default)
+                g_main_loop_quit(_loop);
+
+            CloseAll();
+
+            _loop = default;
+        }
+
+        internal void Close(LinuxNativeNotification? linuxNativeNotification)
+        {
+            if (linuxNativeNotification != null)
+            {
+                CloseNotification(linuxNativeNotification);
+                _notifications.Remove(linuxNativeNotification.Id);
+            }
+        }
+    }
+}
---
+++ src/Avalonia.Labs.Notifications/Avalonia.Labs.Notifications/Linux/LinuxNotificationChannelManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.Labs.Notifications.Linux
+{
+    internal class LinuxNotificationChannelManager
+    {
+        public const string DefaultChannel = "default";
+        public const string DefaultChannelLabel = "Notifications";
+
+        private readonly Dictionary<string, NotificationChannel> _channels = new Dictionary<string, NotificationChannel>();
+
+        public NotificationChannel AddChannel(NotificationChannel notificationChannel)
+        {
+            _channels[notificationChannel.Id] = notificationChannel;
+
+            return notificationChannel;
+        }
+
+        public void DeleteChannel(string channel)
+        {
+            if (_channels.TryGetValue(channel, out _))
+            {
+                _channels.Remove(channel);
+            }
+        }
+
+        public NotificationChannel? GetChannel(string id)
+        {
+            if (_channels.TryGetValue(id, out var channel))
+                return channel;
+
+            return null;
+        }
+    }
+}
