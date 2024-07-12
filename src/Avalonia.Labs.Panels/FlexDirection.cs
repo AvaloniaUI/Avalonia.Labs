8eb449e2429f7b095360af0724d2e9804d601ba2
Al12rs <26797547+Al12rs@users.noreply.github.com>
2024-07-12 16:30:48 +0200

    Add doc comments for FlexDirection

--- src/Avalonia.Labs.Panels/FlexDirection.cs
+++ src/Avalonia.Labs.Panels/FlexDirection.cs
@@ -1,10 +1,28 @@
 ﻿namespace Avalonia.Labs.Panels
 {
+    /// <summary>
+    /// Describes the orientation and direction along which items are placed inside the <see cref="FlexPanel"/>
+    /// </summary>
     public enum FlexDirection
     {
+        /// <summary>
+        /// Items are placed along the horizontal axis, starting from the left
+        /// </summary>
         Row,
+        
+        /// <summary>
+        /// Items are placed along the horizontal axis, starting from the right
+        /// </summary>
         RowReverse,
+        
+        /// <summary>
+        /// Items are placed along the vertical axis, starting from the top
+        /// </summary>
         Column,
+        
+        /// <summary>
+        /// Items are placed along the vertical axis, starting from the bottom
+        /// </summary>
         ColumnReverse
     }
 }
