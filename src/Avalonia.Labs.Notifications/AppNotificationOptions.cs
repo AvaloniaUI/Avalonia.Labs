3667fe61f40238b48104708e01474c85874f9b83
Max Katz <maxkatz6@outlook.com>
2024-10-20 02:22:05 -0700

    Move SupportedOSPlatform to getter only

--- src/Avalonia.Labs.Notifications/AppNotificationOptions.cs
+++ src/Avalonia.Labs.Notifications/AppNotificationOptions.cs
@@ -6,33 +6,26 @@ namespace Avalonia.Labs.Notifications;
 
 public class AppNotificationOptions
 {
-#if INCLUDE_WINDOWS || INCLUDE_LINUX
+#if INCLUDE_WINDOWS
     /// <summary>
     /// Application display name for notifications.
     /// If not defined, Application.Name if used.
     /// Is ignored for packaged applications.
     /// </summary>
-    [SupportedOSPlatform("windows")]
-    [SupportedOSPlatform("linux")]
-    public string? AppName { get; init; }
+    public string? AppName { [SupportedOSPlatform("windows")] get; init; }
 
     /// <summary>
     /// Application icon for notifications.
     /// Is ignored for packaged applications.
     /// </summary>
-    [SupportedOSPlatform("windows")]
-    [SupportedOSPlatform("linux")]
-    public string? AppIcon { get; init; }
-#endif
+    public string? AppIcon { [SupportedOSPlatform("windows")] get; init; }
 
-#if INCLUDE_WINDOWS
     /// <summary>
     /// Overrides AppUserModelId used for notifications.
     /// By default is generated from the
     /// Is ignored for packged applications.
     /// </summary>
-    [SupportedOSPlatform("windows")]
-    public string? AppUserModelId { get; init; }
+    public string? AppUserModelId { [SupportedOSPlatform("windows")] get; init; }
 
     /// <summary>
     /// Indicates whether ComServer for receiving notification actions should be disabled.
@@ -40,8 +33,7 @@ public class AppNotificationOptions
     /// Default is false.
     /// Is ignored for packged applications.
     /// </summary>
-    [SupportedOSPlatform("windows")]
-    public bool DisableComServer { get; init; }
+    public bool DisableComServer { [SupportedOSPlatform("windows")] get; init; }
 
     /// <summary>
     /// Windows notifications require each app to registered a unique COM activator.
@@ -50,8 +42,7 @@ public class AppNotificationOptions
     /// <remarks>
     /// Overriding this property is necessary, if you set ToastActivatorCLSID for packaged apps.
     /// </remarks>
-    [SupportedOSPlatform("windows")]
-    public Guid? ComActivatorGuidOverride { get; init; }
+    public Guid? ComActivatorGuidOverride { [SupportedOSPlatform("windows")] get; init; }
 #endif
 
     public IReadOnlyList<NotificationChannel>? Channels { get; init; }
