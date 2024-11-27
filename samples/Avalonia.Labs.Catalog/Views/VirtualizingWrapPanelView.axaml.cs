9018b6459d383e26106ed92c164b6200e07923f5
Tim <tim.uniewski@gmail.com>
2024-11-27 16:35:19 +0100

    Provide a sample

--- samples/Avalonia.Labs.Catalog/Views/VirtualizingWrapPanelView.axaml.cs
+++ samples/Avalonia.Labs.Catalog/Views/VirtualizingWrapPanelView.axaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using System;
+using System.Linq;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Labs.Catalog.ViewModels;
+using Avalonia.Labs.Catalog.Views.SamplePageBase;
+using Avalonia.Labs.Controls;
+using Avalonia.VisualTree;
 
 namespace Avalonia.Labs.Catalog.Views;
 
@@ -10,5 +14,29 @@ public partial class VirtualizingWrapPanelView : UserControl
     {
         InitializeComponent();
     }
+    
+    public VirtualizingWrapPanel? SamplePanel => this.GetVisualDescendants().OfType<VirtualizingWrapPanel>().FirstOrDefault();
 }
 
+public class VirtualizingWrapPanelSampleView() : ControlSampleBase(new VirtualizingWrapPanelView())
+{
+    private VirtualizingWrapPanelView Sample => Content as VirtualizingWrapPanelView ?? throw new InvalidOperationException();
+    private VirtualizingWrapPanelViewModel ViewModel => (Sample.DataContext as VirtualizingWrapPanelViewModel)!;
+    
+    protected override void PreparePropertyGrid()
+    {
+        // Adding some sample properties to play with
+        var panel = Sample.SamplePanel!;
+        SampleProperties.Add(new PropertyGridItem(VirtualizingWrapPanel.SpacingModeProperty, panel));
+        SampleProperties.Add(new PropertyGridItem(VirtualizingWrapPanel.AllowDifferentSizedItemsProperty, panel));
+        SampleProperties.Add(new PropertyGridItem(VirtualizingWrapPanel.StretchItemsProperty, panel));
+        SampleProperties.Add(new PropertyGridItem(VirtualizingWrapPanel.OrientationProperty, panel));
+        SampleProperties.Add(new PropertyGridItem(VirtualizingWrapPanel.IsGridLayoutEnabledProperty, panel));
+
+        // Adding some Buttons with commands
+        var secondaryContent = new StackPanel();
+        secondaryContent.Children.Add(new Button { Content = "Randomize Items", Command = ViewModel.RandomizeItemSizesCommand});
+        secondaryContent.Children.Add(new Button { Content = "Reset Items", Command = ViewModel.ResetItemSizesCommand});
+        SecondaryContent = secondaryContent;
+    }
+}
