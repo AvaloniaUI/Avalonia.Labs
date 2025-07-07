d80bd54d293443a420d295cb5cc266e1c0a0e96e
Tim <tim.uniewski@gmail.com>
2025-07-07 08:37:25 +0200

    WIP
    some fixes

---
+++ samples/Avalonia.Labs.Catalog/Converters/IsEqualToScrollbarVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data.Converters;
+
+namespace Avalonia.Labs.Catalog.Converters;
+
+public class IsEqualToScrollbarVisibilityConverter : IValueConverter
+{
+    public static IsEqualToScrollbarVisibilityConverter Instance { get; } = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.Equals(parameter) ?? false ? ScrollBarVisibility.Visible : ScrollBarVisibility.Disabled;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
