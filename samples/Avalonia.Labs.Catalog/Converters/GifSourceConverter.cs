c5c16541eabed0d64c5c3ce439974cc26f8727a1
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-11-09 12:53:45 -0600

    Fix: GIF sample view should bind via `IGifSource`

---
+++ samples/Avalonia.Labs.Catalog/Converters/GifSourceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Labs.Gif;
+
+namespace Avalonia.Labs.Catalog.Converters
+{
+    public class GifSourceConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case Stream gifSourceStream:
+                    return GifStreamSource.FromStream(gifSourceStream);
+                case string gifSourceUriString:
+                    return GifStreamSource.FromUriString(gifSourceUriString);
+                case Uri gifSourceUri:
+                    return GifStreamSource.FromUri(gifSourceUri);
+                default:
+                    return null;
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return BindingOperations.DoNothing;
+        }
+    }
+}
